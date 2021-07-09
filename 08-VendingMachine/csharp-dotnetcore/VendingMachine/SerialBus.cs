dc6b39c8a10e18c53bb737e0ec095f0a7eb7fbd2
todd flanders <toddfbass@gmail.com>
2021-07-09 06:42:25 -0400

    allow subscribing to the serial bus

--- 08-VendingMachine/csharp-dotnetcore/VendingMachine/SerialBus.cs
+++ 08-VendingMachine/csharp-dotnetcore/VendingMachine/SerialBus.cs
@@ -1,12 +1,25 @@
-﻿namespace VendingMachine
+﻿using System;
+using System.Collections.Generic;
+
+namespace VendingMachine
 {
     public class SerialBus
     {
         private string _msg = string.Empty;
+        private readonly List<Action<string>> _subscribers = new List<Action<string>>();
 
         public void Send(string msg)
         {
             _msg = msg;
+            foreach (var subscriber in _subscribers)
+            {
+                subscriber(msg);
+            }
+        }
+
+        public void Subscribe(Action<string> action)
+        {
+            _subscribers.Add(action);
         }
 
         public string Recv()
