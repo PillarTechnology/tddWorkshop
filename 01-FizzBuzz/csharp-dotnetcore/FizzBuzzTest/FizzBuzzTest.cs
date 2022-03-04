1a5fbee325344e2296941ac55e7130acc7d6d28b
todd flanders <toddfbass@gmail.com>
2022-03-04 16:46:45 -0500

    minor tweak

--- 01-FizzBuzz/csharp-dotnetcore/FizzBuzzTest/FizzBuzzTest.cs
+++ 01-FizzBuzz/csharp-dotnetcore/FizzBuzzTest/FizzBuzzTest.cs
@@ -6,11 +6,11 @@ namespace Katas
     public class FizzBuzzTest
     {
         [Fact]
-        public void ShouldReturn1For1()
+        public void Returns1For1()
         {
-            FizzBuzz fizzBuzz = new FizzBuzz();
-            string actualReturnValue = fizzBuzz.Process(1);
-            string expectedReturnValue = "1";
+            var fizzBuzz = new FizzBuzz();
+            var actualReturnValue = fizzBuzz.Process(1);
+            var expectedReturnValue = "1";
             Assert.Equal(expectedReturnValue, actualReturnValue);
         }
     }
